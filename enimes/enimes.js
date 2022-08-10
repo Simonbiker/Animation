70acb3ea82b87c2bde9f2e0e28e1c6f0e7f0ed7c
Simonbiker <smacr25@gmail.com>
2022-08-10 14:23:44 +0100

    Adding a new folder and images enimes. Setting up new enimes project.

---
+++ enimes/enimes.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
+
+enemy1 = {
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 200
+}
+
+function animate() {
+    
+}
