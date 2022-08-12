1892b1bcfd6f26e5f64e1d9873de8b2f2d511c3f
Simonbiker <smacr25@gmail.com>
2022-08-12 09:37:36 +0100

    Setting up scalable enemies

--- enimes/enimes.js
+++ enimes/enimes.js
@@ -2,14 +2,40 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 100
+const enemiesArray = [];
 
-enemy1 = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200
+class Enemy {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.width = 100;
+        this.height = 100;
+        this.speed = Math.random() * 4 - 2;
+    }
+
+    update() {
+        this.x += this.speed;
+        this.y += this.speed;
+    }
+    draw() {
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
 }
+
+for(let i = 0; i < numberOfEnemies; i++){
+    enemiesArray.push(new Enemy())
+}
+
+
 
 function animate() {
-    
+    ctx.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemiesArray.forEach(enemy => {
+        enemy.update()
+        enemy.draw()
+    })
+    requestAnimationFrame(animate)
 }
+
+animate()
