142220f8d0e13782d2c87d82883a7c821b4d66f5
Simonbiker <smacr25@gmail.com>
2022-08-12 10:00:38 +0100

    Stopping sprites moving off the canvas and improving code.

--- enimes/enimes.js
+++ enimes/enimes.js
@@ -5,21 +5,38 @@ CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 100
 const enemiesArray = [];
 
+
+let gameFrame = 0;
+
 class Enemy {
     constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.width = 100;
-        this.height = 100;
-        this.speed = Math.random() * 4 - 2;
+        this.image = new Image();
+        this.image.src = '/img/enemy1.png'
+        // this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeiht = 155;
+        this.width = this.spriteHeiht / 2.5;
+        this.height = this.spriteHeiht / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        //Math floor is so gameFrame can be divisable by the result
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1); 
     }
 
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
+        // Randomising the movement of each sprite
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 3 - 1.5;
+        // Animate sprites
+        if (gameFrame % this.flapSpeed === 0) {
+         this.frame > 4 ? this.frame = 0 : this.frame++;   
+        }
+        
     }
     draw() {
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+        // ctx.strokeRect(this.x, this.y, this.width, this.height)
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeiht, this.x, this.y, this.width, this.height)
     }
 }
 
@@ -34,7 +51,8 @@ function animate() {
     enemiesArray.forEach(enemy => {
         enemy.update()
         enemy.draw()
-    })
+    });
+    gameFrame++;
     requestAnimationFrame(animate)
 }
 
